1a2d0adbd123c9f1b6608d4e51df32ea16247ef8
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-03-07 16:15:48 +0300

    Update index03.php

--- index03.php
+++ index03.php
@@ -1,8 +1,24 @@
 <?php
 
+/*
 $toplam = 0;
 for($i=1;$i<=10;$i++){
+  $toplam+=$i;
   echo "$i <br/>";
 }
+echo "1-10 toplam: $toplam";
+
+
+/*$sayi = 1;
+while($sayi<=15){
+  $carpim=1;
+  for($i=1;$i<=$sayi;$i++){
+    $carpim*=$i;
+  }
+  echo "Faktoriyel: $carpim";
+}*/
+
+
+
 
 ?>
