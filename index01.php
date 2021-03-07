20488713a21c7a7eb139f297e110afd8473cff40
Erdem <57351007+ErdemGns@users.noreply.github.com>
2021-03-07 15:25:51 +0300

    Update index01.php

--- index01.php
+++ index01.php
@@ -1,5 +1,6 @@
 <?php
 /*
+
 $durum = 5;
 $sayi1 = 10;
 $sayi2 = 5;
@@ -19,27 +20,27 @@ else if($durum==4){
 else{
   echo 'Girilen durum ile uyuşan durum yok!';
 }
-*/
 
-/*
+
+
 $a = 80;
 if($a%2==0 && $a%5==0){
   echo '10\'a bölünür.';
 }else{
   echo 'Bu bir tek sayıdır.';
 }
-*/
 
-/*
+
+
 $a = 80;
 if($a%2==0 || $a%5==0){
   echo '2\'ye ya da 5\'e bölünebiliyor.';
 }else{
   echo 'Bu bir tek sayıdır.';
 }
-*/
 
-/*
+
+
 $a = 40;
 $b = 30;
 if($a>$b || $a==$b){
@@ -48,29 +49,31 @@ if($a>$b || $a==$b){
 else{
   echo 'Error';
 }
-*/
 
-/*
+
+
 $sayi = null;
 if(isset($sayi) && !empty($sayi)){
   echo "\$sayi değişkeninin değeri: $sayi.";
 }else{
   echo "\$sayi değişkeni boş veya tanımlı değil.";
 }
-*/
 
-/*
+
+
 $sayi=6;
 if(isset($sayi) && !empty($sayi))
     echo "\$sayi değişkeni değeri: $sayi.";
 
 else
     echo "\$sayi değişkeni boş.";
-*/
+
 
 
 $sayi = 6;
 echo isset($sayi) && !empty($sayi) ? "\$sayi değişkeinin değeri: $sayi.":"\$sayi değişkeni boş veya tanımlı değil.";
+
+*/
 
 
 
